6afc7fd2958eda0b0e2560c2e4832581a7c028a3
Divyanshu-Vareshney <varshneydivyanshu2904@gmail.com>
2022-11-25 10:18:45 +0530

    login done

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}> {/* HERE */}
+      <App /> {/* Now, App is wrapped in Provider and hence can read from store */}
+    </Provider>
+  </React.StrictMode>,
+
 );
 
 // If you want to start measuring performance in your app, pass a function
